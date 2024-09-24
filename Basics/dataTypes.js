d18a82bfd67a818ce51c1a6dae872782ce33e2e5
RASHI VISHNOI <142437270+Rashivishnoi03@users.noreply.github.com>
2024-09-24 04:22:43 +0000

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+
+//Primitive DataTypes
+
+//number , bigint , string => "" , null => standalone value 
+//boolean => true/false , undefined => not initialized
+//symbol => unique
+
+//Object
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
---
+++ Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "rashi@gmail.com"
+var accountPass = "123456"
+accountCity = "Jaipur"
+let accountState 
+// accountId = 23
+accountEmail = "r@g.com"
+var accountPass="748596"
+accountCity="hyder"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
+
